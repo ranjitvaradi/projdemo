be2f2a59d44a679dd70f926d19e1f98e031a3ceb
edurekaganesh <ganesh@edureka.co>
2020-07-26 09:06:43 +0530

    commit done for 2 files

---
+++ ganesh.java
@@ -0,0 +1,8 @@
+class ganesh{
+
+	 public static void main(String[] args){
+		 System.out.println("This is created by ganesh as demo prog");
+                 System.out.println("Good Morning everyone ");   
+	 }
+
+}
